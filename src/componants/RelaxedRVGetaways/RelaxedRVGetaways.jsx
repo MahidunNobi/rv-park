c2a9e21a7f300f069abd8add43d18fa6fdfbbc9a
MahidunNobi <mdmahidunnobi@gmail.com>
2024-06-28 00:20:21 +0600

    1st Year and Deleted the secound para of relaxedRVGataways

--- src/componants/RelaxedRVGetaways/RelaxedRVGetaways.jsx
+++ src/componants/RelaxedRVGetaways/RelaxedRVGetaways.jsx
@@ -23,15 +23,8 @@ const RelaxedRVGetaways = () => {
             through Wilson, Oklahoma. Our RV resort comes equipped with all of
             the needed amenities to safely park and rest your RV for as long as
             you would like to stay with us. Our fun, kind, and family-oriented
-            community can put your mind at ease when you stay with us in Wilson.
-            <br /> <br />
-            Experience the pinnacle of RV adventures at Wilson Christmas Tree
-            Farm and RV Park. Nestled in the serene landscapes of Wilson
-            Oklahoma our park offers a quiet and peaceful atmosphere , high
-            speed internet , and a welcoming community. Whether you&apos;re
-            looking for a peaceful retreat or a base for your outdoor
-            explorations, Wilson Christmas Tree Farm and RV Park has everything
-            you need for an unforgettable stay.
+            community can put your mind at ease when you stay with us in Wilson.          
+            
           </p>
           <FilledButton />
         </div>
