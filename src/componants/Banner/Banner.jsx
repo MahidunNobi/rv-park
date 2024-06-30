18edd3ce55b19e888e4cba20d3d5ff903fc8707d
MahidunNobi <mdmahidunnobi@gmail.com>
2024-06-30 08:16:08 +0600

    Address Link added to all the locations

--- src/componants/Banner/Banner.jsx
+++ src/componants/Banner/Banner.jsx
@@ -21,8 +21,14 @@ const Banner = () => {
             Experience the perfect blend of convenience and tranquility at
             Wilson Christmas Tree Farm and RV Park, where the beauty of nature
             and the charm of Wilson come together to create a restful and
-            rejuvenating stay just off Highway 70 at 1955 Cimmaron Road Wilson
-            Oklahoma 73463.
+            rejuvenating stay just off Highway 70 at{" "}
+            <a
+              target="_blank"
+              href="https://www.google.com/maps/place/Wilson+Christmas+Tree+Farm+and+RV+Park/@34.1718366,-97.4398719,17z/data=!3m1!4b1!4m6!3m5!1s0x864d49902a2a3d41:0x2e0971523cd6264a!8m2!3d34.1718366!4d-97.4398719!16s%2Fg%2F11vs9bw4dq?entry=ttu"
+              className="text-blue-600"
+            >
+              1955 Cimmaron Road Wilson Oklahoma 73463.{" "}
+            </a>
           </p>
           <FilledButton />
         </div>
